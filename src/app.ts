6677de475284b85aee0aa2965525fa0be80ef7e4
Rahat47 <djrayhan8@gmail.com>
2021-07-13 22:29:19 +0600

    some practice with ts and 3rd party libs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 import ProjectInput from "./components/project-input";
 import ProjectList from "./components/project-list";
+import Product from "./models/product.model";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
+
+const products = [
+    { title: "Book", price: 12.88 },
+    { title: "Car Toy", price: 120.88 },
+];
+
+const loadedProducts = plainToClass(Product, products);
+
+// const prod = new Product("Test", 999);
+
+console.log(loadedProducts);
